f5756720daa016e40e83baa2f792a574bec3a72e
Sharath <sharathwelcomesyouin@gmail.com>
2022-06-20 18:45:17 +0530

    add exit functionality

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ my_screen.title("USA state Games")
 image="blank_states_img.gif"
 my_screen.addshape(image)
 states_data=pandas.read_csv("50_states.csv")
+# val=states_data[states_data["state"]=="Alaska"]
+# above line = get the row of Alaska
 turtle.shape(image)
 
 my_turtle=turtle.Turtle()
@@ -20,14 +22,17 @@ my_turtle.hideturtle()
 i =0
 score=0
 user_double_check=[]
-while i <50:
+while i <5:
     # need to add numbers
     user_value=my_screen.textinput(title=f"{score}/50 States Correct", prompt="Enter another state").title()
     states_data_row=states_data[states_data["state"]==user_value]
+    print(states_data_row)
     # i=9
+    if user_value=="Exit":
+        # new_list=[states for states in states_data_row if states not in states_data_row]
+        break
     if states_data_row.empty:
         i+=1
-
     else: # states_data_row["state"]==user_value:
         x_pos= int(states_data_row["x"])
         y_pos= int(states_data_row["y"])
@@ -40,10 +45,12 @@ while i <50:
             user_double_check.append(user_value)
             score+=1
 
+
         i+=1
     if score==50:
         my_turtle.goto(0,250)
         my_turtle.write("Wow! Super...!", True, align="center", font=('Arial', 12, 'bold'))
+
 my_turtle.goto(x=0,y=250)
 my_turtle.write(f"your final score: {score}", True, align="center", font=('Arial', 12, 'bold'))
 
